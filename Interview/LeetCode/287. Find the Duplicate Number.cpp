40882187f067cedf5f39d01dd27cd02ce87473f9
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2021-09-14 00:58:31 +0600

    slow fast

---
+++ Interview/LeetCode/287. Find the Duplicate Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& v) {
+        int slow=v[0],fast=v[0];
+        do{
+            slow=v[slow];
+            fast=v[v[fast]];
+        }while(slow!=fast);
+        
+        fast=v[0];
+        while(slow!=fast){
+            slow=v[slow];
+            fast=v[fast];
+        }
+        return slow;
+    }
+};
