f1cbd6cee8e48155281c95ed3680f20609a11589
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2022-01-23 20:49:40 +0600

    template

---
+++ Interview/Java/solve.java
@@ -0,0 +1,18 @@
+package newjava;
+
+import java.util.*;
+
+public class Newjava {
+    static Scanner sc = new Scanner(System.in);
+    public static void solve(){
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            solve();
+        }
+    }
+}
