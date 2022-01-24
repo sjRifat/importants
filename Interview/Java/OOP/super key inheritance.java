255caf5e6ac06d00c4dad38435d60d43e6f766af
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2022-01-24 13:49:49 +0600

    programiz

---
+++ Interview/Java/OOP/super key inheritance.java
@@ -0,0 +1,35 @@
+package newjava;
+
+class animal{
+    public void eat(){
+        System.out.println("i can eat");
+    }
+}
+
+class dog extends animal{
+    @Override
+    public void eat(){
+        super.eat();
+        System.out.println("i eat food");
+    }
+    
+    public void bark(){
+        System.out.println("barking.....");
+    }
+}
+
+public class Newjava {
+    
+  public static void main(String[] args) {
+
+    dog labrador = new dog();
+    labrador.eat();
+    labrador.bark();
+  }
+}
+
+/*
+i can eat
+i eat food
+barking.....
+*/
